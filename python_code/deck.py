bdb44aa68f62842ea21a49a4129b8438c61af2cc
Shaa42 <ryanvigoureux@hotmail.com>
2025-04-24 11:23:01 -0300

    Added water/food/wood cards

---
+++ python_code/deck.py
@@ -0,0 +1,15 @@
+class Card():
+    def __init__(self):
+        ...
+        
+class WaterCard(Card):
+    def __init__(self):
+        super().__init__()
+        
+class FoodCard(Card):
+    def __init__(self):
+        super().__init__()
+        
+class WoodCard(Card):
+    def __init__(self):
+        super().__init__()
